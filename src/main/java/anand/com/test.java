3040c3070f7a083a69b0a68268a41c5a9a96407a
ASONIGRA <ASONIGRA@ASONIGRA-LAP.au.oracle.com>
2021-12-02 18:55:30 +1100

    2nd commit

--- src/main/java/anand/com/test.java
+++ src/main/java/anand/com/test.java
@@ -1,7 +1,7 @@
-package anand.com
+package anand.com;
 
 public class test {
-public static void main(string args[] ) {
+public static void main(String args[] ) {
 	System.out.println("Hello World");
 }
 }
